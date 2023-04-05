e5a0e2a9bef77da4eba861867888374aeba05fb3
Angel Zúñiga Navarro <angel.zuna@icloud.com>
2023-04-05 08:37:09 +0000

    updated Page and globals, changed tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,52 @@
+"use client";
 import React from "react";
+
+interface InputProps {
+	type: string;
+	placeholder: string;
+}
+const Input = ({ type, placeholder }: InputProps): JSX.Element => {
+	return (
+		<input
+			type={type}
+			className="
+				bg-transparent
+				border-b
+				border-b-white
+				px-4
+				text-center
+				mt-16
+			"
+			placeholder={placeholder}
+		/>
+	);
+};
+
+interface ButtonProps {
+	children: JSX.Element | JSX.Element[] | string;
+}
+const Button = ({ children }: ButtonProps): JSX.Element => {
+	return (
+		<button
+			className="
+				bg-purple-500
+				hover:bg-purple-800
+				hover:transition-[background]
+				ease-out
+				duration-100
+				px-4 
+				py-2
+				rounded-md
+				min-w-[90px]
+			"
+			onClick={e => {
+				e.preventDefault();
+			}}
+		>
+			{children}
+		</button>
+	);
+};
 
 interface SectionProps {
 	children: JSX.Element;
@@ -20,46 +68,19 @@ const Page = (): JSX.Element => {
 					items-center
 					bg-white/10
 					flex-col
-					shadow-2xl
-					shadow-white
+					shadow-center
+					shadow-[#ffffff55]
 				"
 			>
-				<input
-					type="text"
-					className="
-						bg-transparent
-						border-b
-						border-b-white
-						px-4
-						text-center
-					"
-					placeholder="Username"
-				/>
-				<input
-					type="password"
-					className="
-						bg-transparent
-						border-b
-						border-b-white
-						px-4
-						text-center
-						mt-16
-					"
-					placeholder="Password"
-				/>
-				<button
-					className="
-						mt-16 
-						border 
-						border-white 
-						px-4 
-						py-2
-					"
-				>
-					Log in
-				</button>
+				<Input type="text" placeholder="Username" />
+				<Input type="password" placeholder="Password" />
+				<div className="mt-16 w-full flex justify-evenly">
+					<Button>Log in</Button>
+					<Button>Register</Button>
+				</div>
 			</form>
 		</section>
 	);
 };
+
 export default Page;
---
+++ tailwind.config.ts
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = {
+	"global-text": "#fffdf0",
+	purple: {
+		50: "#dfdce6",
+		100: "#c1b6de",
+		200: "#a18ed1",
+		300: "#856dc2",
+		400: "#694ead",
+		500: "#4f3494",
+		600: "#381f78",
+		700: "#261259",
+		800: "#160838",
+		900: "#070214",
+	},
+	"gradient-purple": "#1c032e",
+};
+
+const config = {
+	content: [
+		"./app/**/*.{js,ts,jsx,tsx}",
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./libs/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			boxShadow: {
+				center: "0 0 20px #00000055",
+			},
+			colors,
+		},
+	},
+
+	plugins: [],
+};
+module.exports = config;
